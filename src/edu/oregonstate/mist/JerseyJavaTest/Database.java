31f7696af306d9f3176996e364d9829fe832ccd0
Taylor Brown <browtayl@onid.oregonstate.edu>
2015-01-15 13:52:00 -0800

    simulate database

---
+++ src/edu/oregonstate/mist/JerseyJavaTest/Database.java
@@ -0,0 +1,83 @@
+package edu.oregonstate.mist.JerseyJavaTest;
+
+import java.util.HashMap;
+
+public class Database {
+
+	HashMap<Integer,Employee> db;
+
+	/**
+	 * Initialize the database with sample data
+	 */
+	public Database() {
+		db = new HashMap<Integer,Employee>();
+
+		initialize(db);
+	}
+
+	/**
+	 * Read Employee
+	 *
+	 * @param	id	unique identifier
+	 * @return		matching Employee instance or null if none found
+	 */
+	public Employee getEmployeeById(Integer id) {
+		return db.get(id);
+	}
+
+	/**
+	 * Create or Update Employee
+	 *
+	 * @param	employee	instance to be added to database
+	 */
+	public void setEmployee(Employee employee) {
+		db.put(employee.getId(), employee);
+
+		return;
+	}
+
+	/**
+	 * Initialize database with sample data
+	 *
+	 * @param	db	HashMap<Integer,Employee> variable or instance
+	 */
+	private void initialize(HashMap<Integer,Employee> db) {
+		Integer i;
+		Employee e;
+
+		i = 1;
+		e = new Employee(i);
+		e.setOsuId("123456789");
+		e.setLastName("Doe");
+		e.setFirstName("John");
+		e.setMiddleInitial("A");
+		e.setOnidLoginId("dojo");
+		e.setEmailAddress("dojo@onid.orst.edu");
+		e.setEmployeeStatus("A");
+		db.put(i, e);
+
+		i = 2;
+		e = new Employee(i);
+		e.setOsuId("234567891");
+		e.setLastName("Doe");
+		e.setFirstName("Jane");
+		e.setMiddleInitial("B");
+		e.setOnidLoginId("doja");
+		e.setEmailAddress("doja@onid.orst.edu");
+		e.setEmployeeStatus("A");
+		db.put(i, e);
+
+		i = 3;
+		e = new Employee(i);
+		e.setOsuId("345678912");
+		e.setLastName("Smith");
+		e.setFirstName("George");
+		e.setMiddleInitial("R");
+		e.setOnidLoginId("smge");
+		e.setEmailAddress("smge@onid.orst.edu");
+		e.setEmployeeStatus("A");
+		db.put(i, e);
+
+		return;
+	}
+}
